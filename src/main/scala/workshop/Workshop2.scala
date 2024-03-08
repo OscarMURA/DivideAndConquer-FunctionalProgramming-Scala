a87d2f4724917a5fcc53f061e4be266f796eb82b
Luis Rojas <luis.manuel.rojas71@gmail.com>
2024-03-08 11:16:34 -0500

    Implementation of Workshop2 methods, with its corresponding interface and tests: problem of quicksort with 3 partitions

--- src/main/scala/workshop/Workshop2.scala
+++ src/main/scala/workshop/Workshop2.scala
@@ -1,6 +1,41 @@
 package workshop
 
-object Workshop2 {
+object Workshop2 extends InterfaceWorkshop2 {
+  def main(args: Array[String]): Unit = {
+    val w = List(1, 4, 2, 5, 7, 23, 5, 1, 3, 5)
+    val myList: List[Int] = updatedQuickSort(w)
+    println(myList)
+  }
+
+
+  override def equalList(w: List[Int], pivot: Int): List[Int] =
+    w match {
+      case `pivot` :: tail => pivot :: equalList(tail, pivot)
+      case _ :: tail => equalList(tail, pivot)
+      case Nil => Nil
+    }
+
+  override def greaterList(w: List[Int], pivot: Int): List[Int] =
+    w match {
+      case head :: tail if head > pivot => head :: greaterList(tail, pivot)
+      case _ :: tail => greaterList(tail, pivot)
+      case Nil => Nil
+    }
+
+  override def lowerList(w: List[Int], pivot: Int): List[Int] =
+    w match {
+      case head :: tail if head < pivot => head :: lowerList(tail, pivot)
+      case _ :: tail => lowerList(tail, pivot)
+      case Nil => Nil
+    }
+
+  override def updatedQuickSort(w: List[Int]): List[Int] =
+    w match {
+      case Nil => Nil
+      case head :: tail =>
+        updatedQuickSort(lowerList(w, head)) ::: equalList(w, head) ::: updatedQuickSort(greaterList(w, head))
+    }
+  
   
   
   
