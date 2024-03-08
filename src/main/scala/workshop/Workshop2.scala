d8f99b4b06f6a67a4225fe864edf93250b2e5a3c
diegopolancolozano <diegoarmandopolancolozano@gmail.com>
2024-03-08 09:07:42 -0500

    added workshop files

---
+++ src/main/scala/workshop/Workshop2.scala
@@ -0,0 +1,7 @@
+package workshop
+
+object Workshop2 {
+  
+  
+  
+}
--- src/test/scala/Testing.scala
+++ src/test/scala/Testing.scala
@@ -1,13 +1,14 @@
 import munit.FunSuite
 
 class Testing extends munit.FunSuite {
-  test("Problem 1"){
-
-
-  }
-  test("Problem 2"){
+  test("Problem 1") {
 
   }
+
+  test("Problem 2") {
+
+  }
+  
   test("Problem 3"){
     
   }
