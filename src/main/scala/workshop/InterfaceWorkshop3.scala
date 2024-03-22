b4b1af3bb8091d2186427c6a7eed90d6e9dc8262
Oscar Munoz Ramirez <1083870123@u.icesi.edu.co>
2024-03-21 20:58:12 -0500

    Add small fixed of bucketSort

--- src/main/scala/workshop/InterfaceWorkshop3.scala
+++ src/main/scala/workshop/InterfaceWorkshop3.scala
@@ -2,5 +2,5 @@ package workshop
 
 trait InterfaceWorkshop3 {
   def BucketSort(arr: List[Int], fuction: (Int, Int) => Boolean): List[Int]
-  def bucketSortGeneric[T:Numeric](arr: List[T],fuction: (T, T) => Boolean, funtionInterval: (T, T, Int) => Int, maxValue: T): List[T]
-  }
+  def bucketSortGeneric[T](arr: List[T],fuction: (T, T) => Boolean, funtionInterval: (T, T, Int) => Int, maxValue: T): List[T]
+}
--- src/main/scala/workshop/Workshop3.scala
+++ src/main/scala/workshop/Workshop3.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 object Workshop3 extends InterfaceWorkshop3{
 
-  def bucketSortGeneric[T:Numeric](arr: List[T],fuction: (T, T) => Boolean, funtionInterval: (T, T, Int) => Int, maxValue: T): List[T] = {
+  def bucketSortGeneric[T](arr: List[T],fuction: (T, T) => Boolean, funtionInterval: (T, T, Int) => Int, maxValue: T): List[T] = {
     if (arr.isEmpty) return List[T]()
     //case head :: tail => if (x <= head) x :: sortedList else head :: insertSort(x, tail)
 
@@ -27,6 +27,31 @@ object Workshop3 extends InterfaceWorkshop3{
     val maxVal: T = maxValue
     val numBuckets = arr.length
     val buckets: List[List[T]] = List.fill(10000)(Nil)
+    val sortedBuckets = divideIntoBuckets(arr, buckets, maxVal)
+    sortedBuckets.flatten
+  }
+
+  def bucketSortGeneric[T: Numeric](arr: List[T], fuction: (T, T) => Boolean, funtionInterval: (T, T, Int) => Int, maxValue: T, minValue: T): List[T] = {
+    if (arr.isEmpty) return List[T]()
+
+    def insertSort(x: T, sortedList: List[T]): List[T] = sortedList match {
+      case Nil => List(x)
+      case head :: tail =>
+        if (fuction(x, head)) x :: sortedList
+        else head :: insertSort(x, tail)
+    }
+
+    def divideIntoBuckets(arr: List[T], buckets: List[List[T]], maxVal: T): List[List[T]] = arr match {
+      case Nil => buckets
+      case head :: tail =>
+        val index: Int = (funtionInterval(head, maxVal, buckets.length) - 1) max 0
+        val newBuckets = buckets.updated(index, insertSort(head, buckets(index)))
+        divideIntoBuckets(tail, newBuckets, maxVal)
+    }
+
+    val maxVal: T = maxValue
+    val numBuckets = arr.length
+    val buckets: List[List[T]] = List.fill(numBuckets)(Nil)
     val sortedBuckets = divideIntoBuckets(arr, buckets, maxVal)
     sortedBuckets.flatten
   }
